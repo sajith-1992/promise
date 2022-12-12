2a1fea89f2b25061650bf900263d002a82af0245
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2022-12-12 10:55:39 -0500

    promise way of doing

--- promise.js
+++ promise.js
@@ -1,3 +1,4 @@
+const { resolve } = require('promise')
 const  promise = require('promise')
 
 
@@ -10,8 +11,27 @@ function add(num1,num2){
         resolve(num1+num2)
     })
 }
+function multiply(num1,num2){
+    return new promise((resolve,reject)=>{
+        if(num1==0){
+            reject("first number is zero")}
+            resolve(num1*num2)
+        })
+    }
+function divide(num1,num2){
+    return new promise((resolve,reject)=>{
+        resolve(num1/num2)
+    })
+}
 
-add(0,20).then((sum)=>{
-  console.log(sum)  
-}).catch((err)=>{
+add(10,20).then((sum)=>{
+  console.log(sum)
+  return multiply(sum,sum)  
+}).then((product)=>{
+    console.log(product)
+    return divide(product,10)
+}).then((div)=>{
+    console.log(div)
+})
+.catch((err)=>{
         console.log(err)})
