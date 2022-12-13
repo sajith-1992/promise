62e632553a76f9cf336cfe656c66de168d66877a
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2022-12-13 08:29:59 -0500

    promise .all

--- promise.js
+++ promise.js
@@ -1,37 +1,26 @@
-const { resolve } = require('promise')
-const  promise = require('promise')
+const promise=require('promise')
 
 
-function add(num1,num2){
-    return new promise((resolve,reject)=>{
-        
-        if(num1==0){
-        reject("first number is zero")    
-        }
-        resolve(num1+num2)
+function firstName()
+{
+   return new Promise ((resolve,resject)=>{
+    setTimeout(()=>{
+        resolve('sajith')},3000)
+    
+
     })
+   
 }
-function multiply(num1,num2){
-    return new promise((resolve,reject)=>{
-        if(num1==0){
-            reject("first number is zero")}
-            resolve(num1*num2)
-        })
-    }
-function divide(num1,num2){
-    return new promise((resolve,reject)=>{
-        resolve(num1/num2)
-    })
-}
+function mobileNum(){
+    return new promise ((resolve,reject)=>{
+    setTimeout(() => {
+ resolve('87895723')},2000       
+    
+    )}
+    )}
 
-add(10,20).then((sum)=>{
-  console.log(sum)
-  return multiply(sum,sum)  
-}).then((product)=>{
-    console.log(product)
-    return divide(product,10)
-}).then((div)=>{
-    console.log(div)
-})
-.catch((err)=>{
-        console.log(err)})
+
+    //to call this we need to use promise 
+    promise.all([firstName(),mobileNum()]).then ((result)=>{
+        console.log(result)
+    })
